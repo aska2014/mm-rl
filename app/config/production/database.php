1bbf99d56e049758b7c06607d193779938062335
Kareem Mohamed <kareem3d.a@gmail.com>
2014-07-02 19:51:50 +0200

    Add production configuration

---
+++ app/config/production/database.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return array(
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'master',
+            'username'  => 'kareem3d',
+            'password'  => 'JKASD*IOklqew',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+    )
+);
